fb9db0414d63909536b2bbff05409c989aab122e
Kent Larsson <kent@c8.se>
2025-11-17 14:54:49 +0100

    autocommit complete 1454 (49)

--- DEVOPS25/lektion03/views1.sql
+++ DEVOPS25/lektion03/views1.sql
@@ -22,6 +22,6 @@ SELECT e.EventName, e.EventDate, l.LocationName, l.Address, l.City
     FROM Events e
 JOIN Locations l ON e.LocationID = l.LocationID;
 
-SELECT x.EventName, x.EventDate, LocationName, Address, City
-    FROM Events x
-JOIN Locations ON x.LocationID = Locations.LocationID;
+SELECT e.EventName, e.EventDate, l.LocationName, l.Address, l.City
+    FROM Events e
+JOIN Locations l ON e.LocationID = l.LocationID;
