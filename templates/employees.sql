08586bb1985487d6ba1e515eac5e7fe2ebd6e04d
Kent Larsson <kent@c8.se>
2025-11-19 10:30:08 +0100

    autocommit complete 1030 (08)

--- templates/employees.sql
+++ templates/employees.sql
@@ -21,6 +21,11 @@ SELECT * FROM Employee LEFT JOIN Department ON Employee.City = Department.City;
 --            ^--- left eftersom  ^--- till höger om left
 --         Employee står till vänster om Department på raden
 
+SELECT *
+FROM Employee -- Endast rader som uppfyller ON-villkoret från denna
+LEFT JOIN Department -- Alla rader från denna!
+ON Employee.City = Department.City;
+
 SELECT * FROM Employee RIGHT JOIN Department ON Employee.City = Department.City;
 --            ^--- left eftersom  ^--- till höger om left
 --         Employee står till vänster om Department på raden
